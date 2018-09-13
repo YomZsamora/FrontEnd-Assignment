549f761c81d23dbadbf244a1c493776301feebd6
yomz <samora.y@adzumi.co.ke>
2018-09-13 14:31:51 +0300

    Setting up configs for upload.

--- resources/views/yolomy.blade.php
+++ resources/views/yolomy.blade.php
@@ -83,7 +83,7 @@
                     <div class="yolomy-product active">
                         <div class="row pl15"> <h5 class="pull-left">SIZE&nbsp;<i class="fa fa-chevron-down"></i></h5></div>
                         <div class="row">
-                            <img class="img-fluid" src="{{ URL::asset('images/products/tshirt.png') }}" alt="">                            
+                            <img class="img-fluid mt20" src="{{ URL::asset('images/products/tshirt.png') }}" alt="">                            
                         </div>
                         <div class="row pl15 pr15 mt25">                            
                             <h5 class="pull-left type">T-SHIRT</h5>
@@ -95,7 +95,7 @@
                     <div class="yolomy-product">
                         <div class="row pl15"> <h5 class="pull-left">SIZE&nbsp;<i class="fa fa-chevron-down"></i></h5></div>
                         <div class="row">
-                            <img class="img-fluid" src="{{ URL::asset('images/products/pants.png') }}" alt="">                            
+                            <img class="img-fluid mt20" src="{{ URL::asset('images/products/pants.png') }}" alt="">                            
                         </div>
                         <div class="row pl15 pr15 mt25">                            
                             <h5 class="pull-left type">PANTS FORCLAZ</h5>
@@ -106,7 +106,7 @@
                     <div class="yolomy-product">
                         <div class="row pl15"> <h5 class="pull-left">10LITRI</h5></div>
                         <div class="row">
-                            <img class="img-fluid" src="{{ URL::asset('images/products/backpack.png') }}" alt="">                            
+                            <img class="img-fluid mt20" src="{{ URL::asset('images/products/backpack.png') }}" alt="">                            
                         </div>
                         <div class="row pl15 pr15 mt25">                            
                             <h5 class="pull-left type">BACKPACK</h5>
@@ -123,7 +123,7 @@
                     <div class="yolomy-product">
                         <div class="row pl15"> <h5 class="pull-left">SIZE&nbsp;<i class="fa fa-chevron-down"></i></h5></div>
                         <div class="row">
-                            <img class="img-fluid" src="{{ URL::asset('images/products/giacket.png') }}" alt="">                            
+                            <img class="img-fluid mt20" src="{{ URL::asset('images/products/giacket.png') }}" alt="">                            
                         </div>
                         <div class="row pl15 pr15 mt25">                            
                             <h5 class="pull-left type">GIACKET</h5>
@@ -135,7 +135,7 @@
                     <div class="yolomy-product">
                         <div class="row pl15"> <h5 class="pull-left">SIZE&nbsp;<i class="fa fa-chevron-down"></i></h5></div>
                         <div class="row">
-                            <img class="img-fluid" src="{{ URL::asset('images/products/trekkingshoes.png') }}" alt="">                            
+                            <img class="img-fluid mt20" src="{{ URL::asset('images/products/trekkingshoes.png') }}" alt="">                            
                         </div>
                         <div class="row pl15 pr15 mt25">                            
                             <h5 class="pull-left type">TREKKING SHOES</h5>
@@ -146,7 +146,7 @@
                     <div class="yolomy-product">
                         <div class="row pl15"> <h5 class="pull-left">10LITRI</h5></div>
                         <div class="row">
-                            <img class="img-fluid" src="{{ URL::asset('images/products/tshirt_ladies.png') }}" alt="">                            
+                            <img class="img-fluid mt20" src="{{ URL::asset('images/products/tshirt_ladies.png') }}" alt="">                            
                         </div>
                         <div class="row pl15 pr15 mt25">                            
                             <h5 class="pull-left type">T-SHIRT</h5>
@@ -158,19 +158,70 @@
         </div>
     </section>
 
+    <section class="about-us text-center">
+    	<div class="container">
+	        <h5>ABOUT US</h5>
+	        <p>This where high-fashion clothing is<br> designed and crafted from the heart by<br> an amazing team of craftsmen and<br> designers who understand what fashion is</p>    		
+    	</div>
+    </section>
+
+    <section class="twitter">
+    	<div class="container">
+    		<div class="tweet">    			
+    			<img class="pull-left" src="{{ URL::asset('images/social_icons/twitter_bird.png') }}">
+    			<p>"SALE: Buy anything for 45% off. Use the promo CODE YOLOMY45 when you buy items"</p>
+    		</div>
+    	</div>    	
+    </section>
 
 
-
-
-
-
-
-
-
-
-
-
-
+    
+    <footer>
+	    <div class="container">
+	    	<div class="row">
+		        <div class="col-md-4 brief">
+		            <h4>YOLOMY</h4>
+		            <p class="uppercase">We are Yolomy - we understand <br>fashion and style. We craft out <br>products with love and diligence.</p>
+		        </div>
+		        <div class="col-md-4 social">
+		            <div class="row">
+		                <div class="col-md-12 text-center">
+		                    <div class="social">
+		                        <h4 class="mb25">We are Social</h4>
+		                        <ul class="icon_list">
+		                            <li><img src="{{ URL::asset('images/social_icons/g_plus.png') }}"></li>
+		                            <li><img src="{{ URL::asset('images/social_icons/facebook.png') }}"></li>
+		                            <li><img src="{{ URL::asset('images/social_icons/you_tube.png') }}"></li>
+		                            <li><img src="{{ URL::asset('images/social_icons/flickr.png') }}"></li>
+		                        </ul>
+		                        <ul>
+		                            <li><img src="{{ URL::asset('images/social_icons/skype.png') }}"></li>
+		                            <li><img src="{{ URL::asset('images/social_icons/stumble_upon.png') }}"></li>
+		                            <li><img src="{{ URL::asset('images/social_icons/twitter.png') }}"></li>
+		                            <li><img src="{{ URL::asset('images/social_icons/pinterest.png') }}"></li>
+		                        </ul>
+		                    </div>
+		                </div>
+		            </div>
+		        </div>
+		        <div class="col-md-4 links">
+		            <div class="row">
+		                <div class="col-md-12 text-center">
+		                        <p class="uppercase mb0">Credits | Privacy | About | Contact</p>
+		                        <p class="fs22">Yolomy: We Mean Fashion</p>
+		                </div>
+		            </div>
+		        </div>
+		    </div>
+		    <div class="row">
+		    	<div class="col-md-12">
+			    	<div class="copyright text-center uppercase mt50">
+			    		<p>Copyright 2015. yolomy inc</p>
+			    	</div>		    		
+		    	</div>
+		    </div>
+	    </div>
+	</footer>
 
 
 
