2035bf91210823a0b2081094b0ca1d02f92356f6
yomz <samora.y@adzumi.co.ke>
2018-09-13 10:09:21 +0300

    Styled Lato font-family for <body/>, <p/>, <a/> and Header tags.

--- resources/views/yolomi.blade.php
+++ resources/views/yolomi.blade.php
@@ -8,6 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>TM Apprentice Assignment</title>
 
+    <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Lato:100,200,300,400,600,700,900,200italic,300italic,400italic|Merriweather:300,400,300italic">
     <link rel="stylesheet" href="{{ URL::asset('css/style.css') }}">
     <link rel="stylesheet" href="{{ URL::asset('css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ URL::asset('css/font-awesome.min.css') }}">
