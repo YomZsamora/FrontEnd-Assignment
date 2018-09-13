af29e3fac93041f5123bfdf9d8217eddee0cf812
yomz <samora.y@adzumi.co.ke>
2018-09-13 09:57:15 +0300

    Added CSS link files to Yolomi html. Also linked Lato & Merriweather fonts via external sources.

--- resources/views/yolomi.blade.php
+++ resources/views/yolomi.blade.php
@@ -8,7 +8,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>TM Apprentice Assignment</title>
 
-    
+    <link rel="stylesheet" href="{{ URL::asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('css/font-awesome.min.css') }}">
 </head>
 
 <body>
