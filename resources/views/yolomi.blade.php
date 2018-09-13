f3e77753f545b38f44a9b672772f2edb176784f7
yomz <samora.y@adzumi.co.ke>
2018-09-13 10:41:48 +0300

    Added asset images and also finished header design.

--- resources/views/yolomi.blade.php
+++ resources/views/yolomi.blade.php
@@ -15,9 +15,69 @@
 </head>
 
 <body>
+    <header>
+            <nav class="navbar navbar-transparent">
+              <div class="container">
+                <div class="navbar-header">
+                  <a class="navbar-logo" href="#">Yolomy</a>
+                </div>
+
+                <div class="collapse navbar-collapse"">
+                  <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#HOME">Home</a></li>
+                    <li><a href="#PRODUCTS">Products</a></li>
+                    <li><a href="#"><i class="fa fa-shopping-cart"></i></a></li>
+                  </ul>
+                </div>
+              </div>
+            </nav> 
+
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12 text-center">
+                        <div class="home_text">
+                            <h2>Our Clothing , Your Comfort</h2>
+                            <p>We understand that you need to look good and feel great. <br>It all starts with what you're wearing. </p>                       
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12 text-center">
+                        <div class="scroll_down">
+                        <a href="#"><img src="{{ URL::asset('images/mouse_click.png') }}" alt=""></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>      
+    </header>
 
 
     
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <script src="{{ URL::asset('js/jquery.min.js') }}"></script>
+    <script src="{{ URL::asset('js/bootstrap.min.js') }}"></script>
 
 
 
